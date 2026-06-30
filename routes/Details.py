27c023d1cbf6d3c78a211b610893002b67ae540f
ajaybhattnagar <62066694+ajaybhattnagar@users.noreply.github.com>
2026-06-30 19:51:42 -0400

    Add files via upload

--- routes/Details.py
+++ routes/Details.py
@@ -10,6 +10,11 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 import psycopg2
 
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart, MIMEBase
+from email import encoders
+
 with open ('config.json') as f:
     configData = json.load(f)
 
@@ -21,7 +26,6 @@ conn = psycopg2.connect(
     password=configData['postgress_password']
 )
  
-ORDER_FORM_PATH = configData['ORDER_FORM_PATH']
 # ORDER_FORM_PATH = "C:\\test"
 
 details_blueprint = Blueprint('details_blueprint', __name__)
@@ -35,56 +39,60 @@ limiter = Limiter(
     storage_uri="memory://",
 )
 
-
-@details_blueprint.route("/api/v1/upload", methods=['POST'])
-@limiter.limit("10 per minute")
-def upload_order_form():
+def send_email_order_form(email, customer_id, ship_to_name, attachments=[]):
     try:
-        # code to accept form data and save to file
-        customer_id = request.form.get('customerid')
-        name = request.form.get('name')
-        lastname = request.form.get('lastName')
-        email = request.form.get('email')
-        notes = request.form.get('notes')
-        files = request.files.getlist('files')   
+        html_subject = "Order Form Submission Confirmation"
+        body = """
+        <html>
+            <head>
+            </head>
+            <body>
 
-        fileNameList = []           
+                <p>Your order has been received and is being processed. Please wait for an order acknowledgment email.</p>
+                <p><span class="label">Customer ID:</span> {customer_id}</p>
+                <p><span class="label">Ship To Name:</span> {ship_to_name}</p>
 
-        # code to save files to disk
-        # Create a folder in the name of first name and last name and email under the uploads folder
-        folder_name = f"{customer_id}_{name}_{email}"
-        folder_path = f"{ORDER_FORM_PATH}/{folder_name}"
-        os.makedirs(folder_path, exist_ok=True)
-        for file in files:
-            file.save(os.path.join(folder_path, file.filename))
-            fileNameList.append(file.filename)
+                <br>
 
-        # Save notes as text file
-        # Chek if notes is empty
-        if notes is None:
-            notes = "No notes provided"
+                <p>Thank you for your business!</p>
 
-        try:
-            # convert notes to dictionary
-            notes = simplejson.loads(notes)
-            notes_df = pd.DataFrame.from_dict(notes, orient='index')
-            notes_df.to_csv(f"{folder_path}/NOTES.csv", index=True)
-        except simplejson.errors.JSONDecodeError:
-            notes = {"notes": notes}
+                <p>Best regards,<br></p>
 
-        # Generate a random order number
-        order_number = ''.join(random.choices('0123456789', k=6))
+            </body>
+            </html>
+        """.format(customer_id=customer_id, ship_to_name=ship_to_name)
 
-        # Send an email to the customer
-        send_email_with_html(customerid = customer_id, 
-                            name = name, 
-                            order_confirmation = order_number, 
-                            email = email,
-                            attachedFilesList = fileNameList
-                            )
-        return {"message": "Form submitted successfully!", "name": name}, 200
+        print(f"Sending email to {email} with attachments: {attachments}")
+
+        msg = MIMEMultipart()
+        msg['From'] = configData['smtp_user']
+        msg['To'] = email
+        msg['Cc'] = ''
+        msg['Subject'] = html_subject
+        msg.attach(MIMEText(body, 'html'))
+
+        # Attach all the files from attachments list
+        for file_path in attachments:
+            if os.path.isfile(file_path):
+                with open(file_path, 'rb') as attachment:
+                    part = MIMEBase('application', 'octet-stream')
+                    part.set_payload(attachment.read())
+                    encoders.encode_base64(part)
+                    part.add_header('Content-Disposition', f'attachment; filename={os.path.basename(file_path)}')
+                    msg.attach(part)
+
+        server = smtplib.SMTP(configData['smtp_host'], configData['smtp_port'])
+        server.starttls()
+        server.login(configData['smtp_user'], configData['smtp_password'])
+        recipients = email
+        server.sendmail(configData['smtp_user'], recipients , msg.as_string())
+        server.quit()
+
+        return True
+
     except Exception as e:
-        return {"message": "An error occurred", "error": str(e)}, 500
+        print(str(e))
+        return False
 
 
 @details_blueprint.route("/api/v1/price_list", methods=['GET'])
@@ -176,7 +184,7 @@ def upload_excel():
         if not file:
             return {"message": "No file uploaded"}, 400
 
-        folder_path = os.path.join(ORDER_FORM_PATH)
+        folder_path = os.path.join(os.getcwd(), "ORDER_FORMS")
         os.makedirs(folder_path, exist_ok=True)
 
         file_path = os.path.join(folder_path, file.filename)
@@ -184,10 +192,15 @@ def upload_excel():
 
         # optional fields safely
         notes = request.form.get("notes")
-        name = request.form.get("name")
+        email = request.form.get("email")
+        customer_id = request.form.get("customer")
+        ship_to_name = request.form.get("name")
 
         if not notes:
             notes = "No notes provided"
+
+        # Send email with the file attached
+        send_email_order_form(email=email, customer_id=customer_id, ship_to_name=ship_to_name, attachments=[file_path])
 
         try:
             notes_json = simplejson.loads(notes)
